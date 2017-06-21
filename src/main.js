073e01f84a37519d8830d99ca265bfeb95c8f856
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-06-21 08:30:42 +0530

    adding filter completed

--- src/main.js
+++ src/main.js
@@ -25,17 +25,33 @@ new Vue({
     components: {
         'movie-list': {
             template: `<div id="movie-list">
-                            <div v-for="movie in movies" class="movie">
+                            <div v-for="movie in filteredMovies" class="movie">
                                 {{movie.title}}
                             </div>
                         </div>`,
             data() {
                 return {
                     movies: [
-                        { title: 'Pulp Fiction' },
-                        { title: 'Home Alone' },
-                        { title: 'Austin Powers' }
+                        { title: 'Pulp Fiction', genre: genres.CRIME },
+                        { title: 'Home Alone', genre: genres.COMEDY},
+                        { title: 'Austin Powers', genre: genres.COMEDY}
                     ]
+                }
+            },
+            props: ['genre', 'time'],
+            methods:{
+                moviePassesGenreFilter(movie){
+                    if(!this.genre.length){
+                        return true;
+                    }
+                    else{
+                        return this.genre.find(genre => movie.genre === genre);
+                    }
+                }
+            },
+            computed: {
+                filteredMovies(){
+                    return this.movies.filter(this.moviePassesGenreFilter);
                 }
             }
         },
