8369f4bc67e36c97ce9ee8748e73e9d7944ffd82
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-06-21 11:39:29 +0530

    changes to new single file components

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -9,6 +9,7 @@
 
 <<script>
 import genres from '../util/genres';
+import CheckFilter from './CheckFilter.vue';
 
 export default {
     data() {
@@ -22,24 +23,7 @@ export default {
         }
     },
     components: {
-        'check-filter': {
-            data() {
-                return {
-                    checked: false
-                }
-            },
-            props: ["title"],
-            template: `<div v-bind:class="{'check-filter': true, active: checked}" v-on:click="checkFilter">
-                            <span class="checkbox"></span>
-                            <span class="check-filter-title">{{title}}</span>
-                        </div>`,
-            methods: {
-                checkFilter() {
-                    this.checked = !this.checked;
-                    this.$emit('check-filter', 'genre', this.title, this.checked);
-                }
-            }
-        }
+        CheckFilter
     }
 }
 </script>
